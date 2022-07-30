a4885c14083ce1d5e1609460aa4f63aada60c333
koeit <code@koeit.com>
2022-07-30 22:38:36 +0200

    added ant.design & tree base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TreeBase from './Components/TreeBase';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TreeBase />
+    </>
   );
 }
 
---
+++ src/Components/TreeBase.tsx
@@ -0,0 +1,42 @@
+import { DataNode } from 'antd/lib/tree';
+import DirectoryTree, { DirectoryTreeProps } from 'antd/lib/tree/DirectoryTree';
+import React from 'react'
+
+const treeData: DataNode[] = [
+  {
+    title: 'parent 0',
+    key: '0-0',
+    children: [
+      { title: 'leaf 0-0', key: '0-0-0', isLeaf: true },
+      { title: 'leaf 0-1', key: '0-0-1', isLeaf: true },
+    ],
+  },
+  {
+    title: 'parent 1',
+    key: '0-1',
+    children: [
+      { title: 'leaf 1-0', key: '0-1-0', isLeaf: true },
+      { title: 'leaf 1-1', key: '0-1-1', isLeaf: true },
+    ],
+  },
+];
+
+export default function TreeBase() {
+  const onSelect: DirectoryTreeProps['onSelect'] = (keys, info) => {
+    console.log('Trigger Select', keys, info);
+  };
+
+  const onExpand: DirectoryTreeProps['onExpand'] = (keys, info) => {
+    console.log('Trigger Expand', keys, info);
+  };
+  
+  return (
+    <DirectoryTree
+      multiple
+      defaultExpandAll
+      onSelect={onSelect}
+      onExpand={onExpand}
+      treeData={treeData}
+    />
+  )
+}
