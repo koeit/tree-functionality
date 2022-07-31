615d5cc6f1fe6359ec6ac644f0cedcb918aafdfb
koeit <code@koeit.com>
2022-07-31 08:18:37 +0200

    added empty components to add tree nodes and show selected tree node info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import TreeBase from './Components/TreeBase';
+import CreateNewTreeNodes from './Components/CreateNewTreeNodes';
+import ShowSelectedTreeNodeInfo from './Components/ShowSelectedTreeNodeInfo';
 
 function App() {
   return (
     <>
+      <CreateNewTreeNodes />
       <TreeBase />
+      <ShowSelectedTreeNodeInfo />
     </>
   );
 }
