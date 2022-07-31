b1da05afeed4f19652b6056e52a3c9881c6a3f49
koeit <dominik@koeit.de>
2022-07-31 11:59:43 +0200

    added basic input functionality

--- src/Components/CreateNewTreeNodes.tsx
+++ src/Components/CreateNewTreeNodes.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import { Button, Form, Input } from "antd";
+import "./CreateNewTreeNodes.css";
 
 export default function CreateNewTreeNodes() {
+  const [form] = Form.useForm<{}>();
+
   return (
-    <div>CreateNewTreeNodes</div>
-  )
+    <>
+      <h2>Add Tree Nodes</h2>
+      <Form form={form} name="useForm" layout="vertical">
+        <Form.Item name="root_node" noStyle>
+            <Input
+              prefix="Root Node:"
+              style={{ width: "calc(100% - 200px)" }}
+              size="middle"
+              placeholder="node name"
+            />
+        </Form.Item>
+        <Button
+          htmlType="button"
+          type="default"
+          size="middle"
+          style={{ width: "200px" }}
+          onClick={() => {
+            alert(form.getFieldValue("root_node"))
+          }}
+        >
+          Add
+        </Button>
+        <Form.Item name="child_node" noStyle>
+            <Input
+              prefix="Child Node:"
+              style={{ width: "calc(100% - 200px)" }}
+              size="middle"
+              placeholder="node name"
+            />
+        </Form.Item>
+        <Button
+          htmlType="button"
+          type="default"
+          size="middle"
+          style={{ width: "200px" }}
+          onClick={() => {
+            alert(form.getFieldValue("child_node"))
+          }}
+        >
+          Add
+        </Button>
+      </Form>
+    </>
+  );
 }
