6984ef9e1e46db331446d2a6d89f713e6db696bf
koeit <dominik@koeit.de>
2022-07-31 20:24:29 +0200

    added some recursive methods

--- src/Store/TreeBaseStore.ts
+++ src/Store/TreeBaseStore.ts
@@ -1,16 +1,88 @@
-
 // https://www.youtube.com/watch?v=nGZCL6Wd_zQ
+import { DataNode } from "antd/lib/tree";
 import { makeAutoObservable } from "mobx";
 import { TreeDataType } from "../Types/TreeData";
 
-class TreeBaseStore {
-    
-    treeData: TreeDataType[] = [];
+function removeNodeByKey(
+  currentTreeData: DataNode[],
+  key: React.Key
+): DataNode[] {
+  // skip map element
+  // https://www.thiscodeworks.com/javascript-how-to-skip-over-an-element-in-map-stack-overflow-javascript/6205d7e4a4f5770015a884f5
 
-    constructor(){
-        makeAutoObservable(this)
+  return currentTreeData
+    .filter((node) => {
+      if (node.key === key) {
+        return false; // skip
+      }
+
+      return true;
+    })
+    .map((node) => {
+      if (node.children) {
+        return {
+          ...node,
+          children: removeNodeByKey(node.children, key),
+        };
+      }
+
+      return node;
+    });
+}
+
+function renameNodeByKey(
+  currentTreeData: DataNode[],
+  key: React.Key,
+  newNodeName: JSX.Element
+): DataNode[] {
+  return currentTreeData
+    .filter((node) => {
+      if (node.key === key) {
+        node.title = newNodeName;
+      }
+
+      return true;
+    })
+    .map((node) => {
+      if (node.children) {
+        return {
+          ...node,
+          children: renameNodeByKey(node.children, key, newNodeName),
+        };
+      }
+
+      return node;
+    });
+}
+
+function updateTreeData(
+  currentTreeData: DataNode[],
+  key: React.Key,
+  children: DataNode[]
+): DataNode[] {
+  return currentTreeData.map((node) => {
+    if (node.key === key) {
+      return {
+        ...node,
+        children,
+      };
     }
+    if (node.children) {
+      return {
+        ...node,
+        children: updateTreeData(node.children, key, children),
+      };
+    }
+    return node;
+  });
+}
 
+class TreeBaseStore {
+  treeData: TreeDataType[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 }
 const treeBaseStore = new TreeBaseStore();
 export default treeBaseStore;
