4e711031af834faa9ef96dc95ae91099914a6c03
Dominik Königseder <95132236+koeit@users.noreply.github.com>
2022-08-02 22:20:01 +0200

    Develop (#2)
    * changed license
    
    * added appendRootNode
    
    * append root nodes and map treeData to styledTreeData
    
    * changed readme

--- src/Store/TreeBaseStore.ts
+++ src/Store/TreeBaseStore.ts
@@ -2,6 +2,7 @@
 import { DataNode } from "antd/lib/tree";
 import { makeAutoObservable } from "mobx";
 import { TreeDataType } from "../Types/TreeData";
+import MapTreeDataToStyledTreeData from "../utils/MapTreeDataToStyledTreeData";
 
 function removeNodeByKey(
   currentTreeData: DataNode[],
@@ -77,11 +78,27 @@ function updateTreeData(
   });
 }
 
+const appendRootNodes = (rootNodesData: TreeDataType[], currentTreeData: TreeDataType[]) : void => {
+  rootNodesData.forEach(rootNode => {
+    currentTreeData.push(rootNode);
+  })
+}
+
 class TreeBaseStore {
   treeData: TreeDataType[] = [];
+  styledTreeData: DataNode[] = [];
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  appendRootNode(rootNodeData: TreeDataType[]) {
+    // append rootNodeData to this.treeData because of call by reference
+    appendRootNodes(rootNodeData, this.treeData);
+  }
+
+  mapTreeDataToStyledTreeData(){
+    this.styledTreeData = MapTreeDataToStyledTreeData([...this.treeData]);
   }
 }
 const treeBaseStore = new TreeBaseStore();
---
+++ src/utils/MapTreeDataToStyledTreeData.tsx
@@ -0,0 +1,24 @@
+import { DataNode } from 'antd/lib/tree'
+import { TreeDataType } from '../Types/TreeData';
+
+const goThroughEveryNodeInTreeData = (currentTreeData: TreeDataType[]) : DataNode[] => {
+    let styledTreeData: DataNode[] = []
+
+    currentTreeData.forEach(node => {
+        if (node.hasChildren) {
+            styledTreeData.push({
+                title: <span style={{color: "red"}}>{node.name}</span>, key: node.id, selectable: false, children: goThroughEveryNodeInTreeData(node.children!)
+            })
+        } else {
+            styledTreeData.push({
+                title: <span style={{color: "blue"}}>{node.name}</span>, key: node.id, selectable: false, children: []
+            })
+        }
+    });
+
+    return styledTreeData
+}
+
+export default function MapTreeDataToStyledTreeData(currentTreeData: TreeDataType[]) : DataNode[] {
+  return goThroughEveryNodeInTreeData(currentTreeData);
+}
